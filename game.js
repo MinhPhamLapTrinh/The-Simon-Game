76aa91c1368442bf55c6cb81eea9ab0a645641f3
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-01-13 19:08:08 +0700

    initial commit

---
+++ game.js
@@ -0,0 +1,82 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let count = 0;
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  count += 1;
+  $("#level-title").text("Level " + count);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + count);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      console.log(userClickedPattern);
+      console.log(gamePattern);
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  started = false;
+  count = 0;
+  gamePattern = [];
+}
